855faa0a11682ce6f3ecd27e79fde1bbb9a90366
SwastikPurathepparambil <72681039+SwastikPurathepparambil@users.noreply.github.com>
2021-02-27 19:59:02 -0800

    Update swastik_app.py

--- Swastik/swastik_app.py
+++ Swastik/swastik_app.py
@@ -1,33 +1,37 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 from models import setup_db, db, Resident, BoardMember, BoardInfo, ExpensesItem, HoaUpdates
 app = Flask(__name__)
 setup_db(app)
 
 @app.route('/')
 def index():
-    return render_template('front_page.html')
+    return render_template('dashboards/front_page.html')
 
 @app.route('/signup')
 def sign_up():
-    return render_template('signup/board_members/bm_signup.html')
+    return render_template('dashboards/login.html')
 
-@app.route('/dashboard/board_members/<int:board_id>')
+@app.route('/dashboards/board/<int:board_id>')
 def show_hoa_dashboard(board_id):
     board = BoardInfo.query.get(board_id)
     board_members = BoardMember.query.filter(board.id == BoardMember.board_group_id).with_entities(BoardMember.name, BoardMember.email).all()
-    expenses = ExpensesItem.query.filter(board.id == ExpensesItem.expense_id).with_entities(ExpensesItem.expense_item, ExpensesItem.expense_amount).all()
+    expenses = ExpensesItem.query.filter(board.id == ExpensesItem.expense_list_id).with_entities(ExpensesItem.expense_item, ExpensesItem.expense_amount).all()
+    return render_template('dashboards/board.html', expenses=expenses, board_members=board_members, board_name=board.name)
 
-    return render_template('board_members/dashboard.html', expenses=expenses, board_members=board_members, board_name=board.name)
 
-
-@app.route('dashboard/residents/<int:resident_id')
+@app.route('/dashboards/residents/<int:resident_id>')
 def show_resident_dashboard(resident_id):
     resident = Resident.query.get(resident_id)
     resident_hoa = resident.hoa_group
-    hoa_update = HoaUpdates.query.filter(resident_hoa == HoaUpdates.query.with_entities(HoaUpdates.hoa_group).all()).all()
-    contacts = BoardMember.query.filter(
-        resident_hoa == BoardInfo.query.with_entities(BoardInfo.name).all()
-        ).with_entities(BoardMember.name, BoardMember.email).all()
-    return render_template('residents/dashboard.html', hoa_updates = hoa_update, contacts=contacts)
+    # hoa_update = HoaUpdates.query.filter(resident_hoa == HoaUpdates.query.with_entities(HoaUpdates.hoa_group).all()).all()
+    # contacts = BoardMember.query.filter(resident.id == BoardMember.board_group_id).with_entities(BoardMember.name, BoardMember.email).all()
+    hoa_update = HoaUpdates.query.all()
+    board_members = BoardMember.query.filter(resident.hoa_group == BoardMember.board_group_id)
+    return render_template('dashboards/residents.html', hoa_updates=hoa_update, board_members=board_members)
+
+@app.route('/sendmail')
+def show_sendmail():
+    return render_template('dashboards/sendmailer.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
